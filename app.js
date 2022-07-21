1460e3b7c1666b9b2fbaa42279946de66af00465
mistymidnights <mistydeveloping@gmail.com>
2022-07-21 21:11:24 +0200

    Iteración #1: Interacción con el DOM

---
+++ app.js
@@ -0,0 +1,22 @@
+/* Iteración #1: Interacción con el DOM */
+
+let show = document.querySelector(".showme");
+console.log(show);
+
+let pillados = document.querySelector("#pillado");
+console.log(pillados);
+
+let parrafos = document.querySelectorAll("p")
+console.log(parrafos);
+
+let pokemons = document.querySelectorAll(".pokemon")
+console.log(pokemons);
+
+let testMe = document.querySelector('[data-function="testMe"]')
+console.log(testMe);
+
+
+let atributes = document.querySelectorAll('[data-function="testMe"]');
+atributes.forEach( ( element, index) => {
+    console.log( index == 2 ? element : '' );
+});
